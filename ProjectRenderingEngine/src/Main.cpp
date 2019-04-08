5f4bcfc0d0d5b80a4adb3d4eb07fc25c57427dda
Jeroen Schrader <muismatje3@gmail.com>
2019-04-08 10:02:38 +0200

    Basic VAO, VBO and VBLayout classes

--- ProjectRenderingEngine/src/Main.cpp
+++ ProjectRenderingEngine/src/Main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
-#include <GLEW/glew.h>
+
+#include "Renderer.h"
 #include <GLFW/glfw3.h>
+
+#include "VertexArray.h"
+#include "VertexBuffer.h"
+#include "VertexBufferLayout.h"
 
 int main() {	
 	GLFWwindow* window;
@@ -10,7 +15,7 @@ int main() {
 		return -1;
 
 	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	window = glfwCreateWindow(1280, 720, "Rendering Engine by Jeroen Schrader", NULL, NULL);
 	if (!window)
 	{
 		glfwTerminate();
@@ -24,11 +29,27 @@ int main() {
 		std::cout << "Glew init error" << std::endl;
 	};
 
+	float vertices[9]{
+		-1.0f, -1.0f, 0.0f,
+		0.0f, 1.0f, 0.0f,
+		1.0f, -1.0f, 0.0f
+	};
+
+	VertexArray vao;
+	VertexBuffer vb(vertices, 3 * 3 * sizeof(float));
+	VertexBufferLayout layout;
+
+	layout.Push<float>(3);
+	vao.AddBuffer(vb, layout);
+
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
+		glClearColor(0.3f, 0.4f, 0.8f, 1.0f);
+		
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
---
+++ ProjectRenderingEngine/src/VertexBuffer.cpp
@@ -0,0 +1,25 @@
+#include "VertexBuffer.h"
+#include "VertexBufferLayout.h"
+#include "Renderer.h"
+
+VertexBuffer::VertexBuffer(const void* data, unsigned int size)
+{
+	glGenBuffers(1, &m_Id);
+	glBindBuffer(GL_ARRAY_BUFFER, m_Id);
+	glBufferData(GL_ARRAY_BUFFER, size, data, GL_STATIC_DRAW);
+}
+
+VertexBuffer::~VertexBuffer()
+{
+	glDeleteBuffers(1, &m_Id);
+}
+
+void VertexBuffer::Bind() const
+{
+	glBindBuffer(GL_ARRAY_BUFFER, m_Id);
+}
+
+void VertexBuffer::Unbind() const
+{
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
