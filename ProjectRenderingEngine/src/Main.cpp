3c4ab351736c1d1d0ec903904bc4c4d0e98b944e
Jeroen Schrader <muismatje3@gmail.com>
2019-04-08 15:56:56 +0200

    Added IndexBuffer

--- ProjectRenderingEngine/src/Main.cpp
+++ ProjectRenderingEngine/src/Main.cpp
@@ -6,6 +6,7 @@
 #include "VertexArray.h"
 #include "VertexBuffer.h"
 #include "VertexBufferLayout.h"
+#include "IndexBuffer.h"
 
 int main() {	
 	GLFWwindow* window;
@@ -29,27 +30,54 @@ int main() {
 		std::cout << "Glew init error" << std::endl;
 	};
 
-	float vertices[9]{
-		-1.0f, -1.0f, 0.0f,
-		0.0f, 1.0f, 0.0f,
-		1.0f, -1.0f, 0.0f
+	//Rectangle vertices
+	float vertices[]{
+		-0.8f, -0.8f, 0.0f,	//lower left
+		-0.8f,  0.8f, 0.0f, //upper left
+		 0.8f,  0.8f, 0.0f,	//upper right
+		 0.8f, -0.8f, 0.0f	//lower right
 	};
 
-	VertexArray vao;
-	VertexBuffer vb(vertices, 3 * 3 * sizeof(float));
-	VertexBufferLayout layout;
+	unsigned int indices[]{
+		0, 1, 2,
+		2, 3, 0
+	};
 
+	VertexBuffer vb(vertices, 4 * 3 * sizeof(float));
+	VertexBufferLayout layout;
 	layout.Push<float>(3);
+
+	IndexBuffer ib(indices, sizeof(indices) / sizeof(unsigned int));
+
+	VertexArray vao;
 	vao.AddBuffer(vb, layout);
 
+	double lastTime = glfwGetTime();
+	int nbFrames = 0;
+	
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
+		#pragma region Frame/FpsTimer
+		// Measure speed
+		double currentTime = glfwGetTime();
+		nbFrames++;
+		if (currentTime - lastTime >= 1.0) { // If last prinf() was more than 1 sec ago
+			// set new window title and reset timer
+			char buffer[75];
+			sprintf_s(buffer, "%.3f ms/frame | %.2f fps | Rendering Engine by Jeroen Schrader", 1000.0 / double(nbFrames), double(nbFrames));
+			nbFrames = 0;
+			lastTime += 1.0;
+			glfwSetWindowTitle(window, buffer);
+		}
+		#pragma endregion
+			   
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
 		glClearColor(0.3f, 0.4f, 0.8f, 1.0f);
 		
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		ib.Bind();
+		glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(unsigned int), GL_UNSIGNED_INT, nullptr);
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
